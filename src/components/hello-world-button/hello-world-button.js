ce74d37cfa5fc5e89c0d70a5479b7311410ca922
ayorich <kayodele17@yahoo.com>
2021-01-24 13:45:06 +0100

    babel

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,10 +1,12 @@
 import "./hello-world-button.scss";
 
 class HelloWorldButton {
+  buttoncssclass = "hello-world-button";
+
   render() {
     const btn = document.createElement("button");
     btn.innerHTML = "Hello world";
-    btn.classList.add("hello-world-button");
+    btn.classList.add(this.buttoncssclass);
     const body = document.querySelector("body");
     btn.onclick = function () {
       const p = document.createElement("p");
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,17 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"], // scss stylesheet loader
       },
+      {
+        test: /\.js$/, //code transpiller from es6 es7 syntax to old syntax for older browser
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
     ],
   },
 };
