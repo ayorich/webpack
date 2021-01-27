f288e72fe6701e907a9bee1bd3a1d4c23ff31a6d
ayorich <kayodele17@yahoo.com>
2021-01-27 06:38:53 +0100

    multiple-page-serving

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
     // publicPath: "dist/", //sets to auto from webpack 5
-    publicPath: "",
+    publicPath: "/static/",
   },
   mode: "production",
   optimization: {
