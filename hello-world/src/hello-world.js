cca362763d53ef02de61273b1d617cc9c0982d77
ayorich <kayodele17@yahoo.com>
2021-01-30 08:35:29 +0100

    fontawesome-added

--- hello-world/src/hello-world.js
+++ hello-world/src/hello-world.js
@@ -1,8 +1,13 @@
 // import "./index.scss";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
 import Heading from "./components/heading/heading.js";
+
+library.add(faSpinner);
+dom.watch();
 
 const heading = new Heading();
 heading.render("hello world");
