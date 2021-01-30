83b2b1cd5cdff2273105aee762440a604cee98c2
ayorich <kayodele17@yahoo.com>
2021-01-30 07:41:04 +0100

    custom-fonts

--- hello-world/src/hello-world.js
+++ hello-world/src/hello-world.js
@@ -1,3 +1,4 @@
+import "./index.scss";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
 import Heading from "./components/heading/heading.js";
 
--- hello-world/webpack.production.config.js
+++ hello-world/webpack.production.config.js
@@ -41,6 +41,18 @@ module.exports = {
         test: /\.hbs$/,
         use: ["handlebars-loader"],
       },
+      {
+        test: /\.(woff2|woff|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
     ],
   },
   //plugins
